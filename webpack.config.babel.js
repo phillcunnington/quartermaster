ffdfcf450aac6d34f8d73c88fc2d989c890acbdb
Phill Cunnington <phill.cunnington@forgerock.com>
2016-06-15 09:10:19 +0100

    Add webpack to build ui

---
+++ webpack.config.babel.js
@@ -0,0 +1,53 @@
+"use strict";
+
+import dotenv from "dotenv";
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import CleanWebpackPlugin from "clean-webpack-plugin";
+import validate from "webpack-validator";
+dotenv.config();
+
+const PATHS = {
+  app: path.join(__dirname, "app"),
+  build: path.join(__dirname, "public")
+};
+
+const config = {
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: "[name].js"
+  },
+  plugins: [
+    new CleanWebpackPlugin([PATHS.build], {
+      root: process.cwd()
+    }),
+    new HtmlWebpackPlugin({
+      title: "Quartermaster Journal",
+      template: "./app/index.html.template"
+    })
+  ],
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        loaders: ["babel?cacheDirectory"],
+        include: PATHS.app
+      }
+    ]
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    historyApiFallback: true,
+    stats: "errors-only",
+    host: process.env.HOST,
+    port: process.env.PORT
+  }
+};
+
+module.exports = validate(config);
