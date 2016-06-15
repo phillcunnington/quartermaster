b7e0179ab8a84d95eb2f1b9b16e0cba367edaa83
Phill Cunnington <phill.cunnington@forgerock.com>
2016-06-15 09:31:33 +0100

    Display accounts total

--- app/index.js
+++ app/index.js
@@ -1,12 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Backend from "./backend";
+import numeral from "numeral";
+numeral.defaultFormat("$0,0.00");
+numeral.language('en-GB', {
+    delimiters: {
+        thousands: ',',
+        decimal: '.'
+    },
+    currency: {
+        symbol: '£'
+    }
+});
+numeral.language('en-GB');
+
+class AccountsTotal extends React.Component {
+  render() {
+    return (
+      <div id="accountsTotal">
+        Accounts Total: {numeral(this.props.total / 100).format()}
+      </div>
+    );
+  }
+}
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-      Hello
+      <div id="appContainer">
+        <AccountsTotal total={this.props.data.accounts.total} />
       </div>
     );
   }
