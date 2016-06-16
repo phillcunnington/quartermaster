a2dd34f9b75d4213c4a54e4c0c30381d847019b3
Phill Cunnington <phill.cunnington@forgerock.com>
2016-06-16 09:54:22 +0100

    Add Google OAuth2 authentication to all routes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,70 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const logger = require("morgan");
 
-app.use(express.static("public"));
+
+// Passport setup
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_REDIRECT_URI
+  },
+  function(accessToken, refreshToken, profile, done) {
+    done(null, { id: profile.id });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  done(null, { id: id });
+});
+
+
+
+
+app.use(logger(process.env.HTTP_LOGGING_FORMAT));
+
+
+
+
+// Express authentication setup
+const session = require("express-session");
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ["profile"] }));
+
+app.get("/auth/google/callback",
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/");
+  });
+
+function authenticate(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/auth/google");
+}
+
+
+
+// Route declaration
+app.use(authenticate, express.static("public"));
 
 app.listen(port);
