d89ea632baea5cf95e0be9f3f725bfb3773caf90
Phill Cunnington <phill.cunnington@forgerock.com>
2016-07-03 21:09:13 +0100

    Add mongo integration to find users

--- server/index.js
+++ server/index.js
@@ -4,6 +4,15 @@ const port = process.env.PORT || 3000;
 const logger = require("morgan");
 
 
+// Mongo and Mongoose setup
+const mongoose = require("mongoose");
+const User = mongoose.model("Users", {
+  google_id: String
+});
+mongoose.set('debug', true);
+mongoose.connect(process.env.MONGODB_URI);
+
+
 // Passport setup
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
@@ -14,16 +23,30 @@ passport.use(new GoogleStrategy({
     callbackURL: process.env.GOOGLE_REDIRECT_URI
   },
   function(accessToken, refreshToken, profile, done) {
-    done(null, { id: profile.id });
+    console.log("profile: " + JSON.stringify(profile));
+    User.findOne({ "google_id": profile.id })
+      .select("_id google_id")
+      .exec(function(err, user) {
+      if (err) {
+        console.log("got error!");
+      } else if (user) {
+        console.log("found user: " + JSON.stringify(user));
+      } else {
+        console.log("did not find user");
+      }
+      done(err, user);
+    })
   }
 ));
 
 passport.serializeUser(function(user, done) {
-  done(null, user.id);
+  console.log("serialize user: " + JSON.stringify(user));
+  done(null, user);
 });
 
-passport.deserializeUser(function(id, done) {
-  done(null, { id: id });
+passport.deserializeUser(function(user, done) {
+  console.log("deserialize user: " + JSON.stringify(user));
+  done(null, user);
 });
 
 
